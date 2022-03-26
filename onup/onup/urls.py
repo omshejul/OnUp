60330258c91a1fc6f83cb1dafba02a4cc6962b24
Om Shejul <omshejul07@gmail.com>
2022-03-26 15:12:37 +0530

    added home page

--- onup/onup/urls.py
+++ onup/onup/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
+    path('',include('user.urls')),
+    path('adminpage/',include('admin.urls')),
     path('admin/', admin.site.urls),
 ]
