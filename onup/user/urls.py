535c32b088a52aa681a3aceabbc4aee93a1a0478
Om Shejul <omshejul07@gmail.com>
2022-03-27 00:44:11 +0530

    firebase database

--- onup/user/urls.py
+++ onup/user/urls.py
@@ -1,6 +1,10 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('',views.index, name='index'),
     # path('add',views.add,name='add')
 ]
+
+ 
--- onup/user/views.py
+++ onup/user/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+import pyrebase
+ 
+
+config={
+    "apiKey": "AIzaSyCXnY_yxEYdsde2V6-riUxYFTZVn8yKSkw",
+    "authDomain": "onup-codebreak.firebaseapp.com",
+    "databaseURL": "https://onup-codebreak-default-rtdb.firebaseio.com/",
+    "projectId": "onup-codebreak",
+    "storageBucket": "onup-codebreak.appspot.com",
+    "messagingSenderId": "350537350064",
+    "appId": "1:350537350064:web:4af427c4feda7df9bd34754",
+    "measurementId": "G-E43K5HSY33"
+}
+
+
+
+firebase=pyrebase.initialize_app(config)
+authe = firebase.auth()
+database=firebase.database()
+
+
 def index(request):
-    return render(request, 'index.html')
+        #accessing our firebase data and storing it in a variable
+        projectname = database.child('Projectname').get().val()
+    
+        context = {
+            'projectname':projectname
+        }
+        return render(request, 'index.html', context)
